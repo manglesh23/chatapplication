91211120dff552cc75bb32e25499c0b33c2826cd
manglesh23 <mangleshyadav2@gmail.com>
2024-07-09 02:17:15 +0530

    controllers

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -1,17 +1,41 @@
 const { chats } = require("../data/data");
+const User = require("../models/user");
 
+const registerUser = async (req, res) => {
+  const { name, email, password, pic } = req.body;
 
-const registerUser=async(req,res)=>{
-    console.log(req);
-   
-    res.status(200).json({msg:"signup"});
-}
+  if (!name || !email || !password || !pic) {
+    res.status(404);
+    throw new Error("please provide all the data input");
+  }
+  const userExist = await User.findOne({ email: email });
+  if (userExist) {
+    res.status(400);
+    throw new Error("USer Exists, Email should be unique");
+  }
 
-const userlogin=async(req,res)=>{
-    res.status(200).json({msg:chats});
-    // console.log(chats)
-}
+  const user = await User.create({
+    name,
+    email,
+    password,
+    pic,
+  });
 
+  if (user) {
+    res.status(200).json({
+      id: user._id,
+      name: user.name,
+      email: user.email,
+      pic: user.pic,
+    });
+  } else {
+    res.status(400).json({ msg: "failed to register" });
+  }
+};
 
+const userlogin = async (req, res) => {
+  res.status(200).json({ msg: chats });
+  // console.log(chats)
+};
 
-module.exports={registerUser,userlogin}
+module.exports = { registerUser, userlogin };
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
+const bcrypt = require("bcrypt");
 
 const userSchema = mongoose.Schema(
   {
     name: { type: "String", required: true },
-    email: { type: "String", unique: true, required: true },
+    email: { type: "String", unique: true, required: true,unique:true },
     password: { type: "String", required: true },
     pic: {
       type: "String",
-      required: true,
+      
       default:
         "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
     },
