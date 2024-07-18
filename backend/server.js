47820e95dcd22864fe094a7363c9d19cadc1829f
manglesh23 <mangleshyadav2@gmail.com>
2024-07-18 16:03:07 +0530

    socket.io

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const { router } = require("./authrouter/authrouter");
 require("dotenv").config();
 const app = express();
 
-const server = http.createServer(app);      //created the server here
+const server = http.createServer(app);          //created the server here
 const io = require("socket.io")(server);    
 
 app.use(express.json());
@@ -14,7 +14,7 @@ app.use("/", router);
 
 connectdatabase()
   .then(() => {
-    server.listen(7000, () => {             //listenning the server here
+    server.listen(7000, () => {               //listenning the server here
       console.log("listening at 7000");
     });
   })
@@ -27,14 +27,33 @@ connectdatabase()
 
 
   io.on("connection", (socket) => {
-    console.log("A user connected");
-  
-    socket.on("chat message", (msg) => {
-      console.log("message: " + msg);
-      io.emit("chat message", msg);
+    console.log("Connected to socket.io");
+    socket.on("setup", (userData) => {
+      socket.join(userData._id);
+      socket.emit("connected");
     });
   
-    socket.on("disconnect", () => {
-      console.log("A user disconnected");
+    socket.on("join chat", (room) => {
+      socket.join(room);
+      console.log("User Joined Room: " + room);
+    });
+    socket.on("typing", (room) => socket.in(room).emit("typing"));
+    socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+  
+    socket.on("new message", (newMessageRecieved) => {
+      var chat = newMessageRecieved.chat;
+  
+      if (!chat.users) return console.log("chat.users not defined");
+  
+      chat.users.forEach((user) => {
+        if (user._id == newMessageRecieved.sender._id) return;
+  
+        socket.in(user._id).emit("message recieved", newMessageRecieved);
+      });
+    });
+  
+    socket.off("setup", () => {
+      console.log("USER DISCONNECTED");
+      socket.leave(userData._id);
     });
   });
