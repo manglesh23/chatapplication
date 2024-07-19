1a8c6cf68050ea8172518e5310b260951564e327
manglesh23 <mangleshyadav2@gmail.com>
2024-07-19 23:25:53 +0530

    frontend

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,21 @@
 const express = require("express");
-const http=require('http');
+const http = require("http");
 const { connectdatabase } = require("./databaseconnect/databaseconnect");
 const { router } = require("./authrouter/authrouter");
 
 require("dotenv").config();
 const app = express();
 
-const server = http.createServer(app);          //created the server here
-const io = require("socket.io")(server);    
+const server = http.createServer(app); //created the server here
+const io = require("socket.io")(server);
 
 app.use(express.json());
 app.use("/", router);
 
 connectdatabase()
   .then(() => {
-    server.listen(7000, () => {               //listenning the server here
+    server.listen(7000, () => {
+      //listenning the server here
       console.log("listening at 7000");
     });
   })
@@ -25,35 +26,36 @@ connectdatabase()
     };
   });
 
+io.on("connection", (socket) => {
+  console.log("Connected to socket.io");
 
-  io.on("connection", (socket) => {
-    console.log("Connected to socket.io");
-    socket.on("setup", (userData) => {
-      socket.join(userData._id);
-      socket.emit("connected");
-    });
-  
-    socket.on("join chat", (room) => {
-      socket.join(room);
-      console.log("User Joined Room: " + room);
-    });
-    socket.on("typing", (room) => socket.in(room).emit("typing"));
-    socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
-  
-    socket.on("new message", (newMessageRecieved) => {
-      var chat = newMessageRecieved.chat;
-  
-      if (!chat.users) return console.log("chat.users not defined");
-  
-      chat.users.forEach((user) => {
-        if (user._id == newMessageRecieved.sender._id) return;
-  
-        socket.in(user._id).emit("message recieved", newMessageRecieved);
-      });
-    });
-  
-    socket.off("setup", () => {
-      console.log("USER DISCONNECTED");
-      socket.leave(userData._id);
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+    socket.emit("connected");
+  });
+
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("User Joined Room: " + room);
+  });
+
+  socket.on("typing", (room) => socket.in(room).emit("typing"));
+  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
+  socket.on("new message", (newMessageRecieved) => {
+    var chat = newMessageRecieved.chat;
+
+    if (!chat.users) return console.log("chat.users not defined");
+
+    chat.users.forEach((user) => {
+      if (user._id == newMessageRecieved.sender._id) return;
+
+      socket.in(user._id).emit("message recieved", newMessageRecieved);
     });
   });
+
+  socket.off("setup", () => {
+    console.log("USER DISCONNECTED");
+    socket.leave(userData._id);
+  });
+});
---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import Chat from './Chat';
+
+function App() {
+  const userId = "yourUserId"; // Replace with the actual user ID
+
+  return (
+    <div className="App">
+      {/* <Chat userId={userId} /> */}
+      <h1>Chat App</h1>
+    </div>
+  );
+}
+
+export default App;
